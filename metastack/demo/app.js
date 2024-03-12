a10837c34b78a1ebce528aa76876d9d8293396a5
Sunidhi Sharma <sunidhisharma3002@gmail.com>
2024-03-12 23:10:32 +0545

    ERR_HTTP_HEADERS_SENT is to be resolved

--- metastack/demo/app.js
+++ metastack/demo/app.js
@@ -2,22 +2,30 @@ const http = require('http');
 const server = http.createServer((request, response) => {
 
     response.writeHead(200, {'Content-type': 'text/html'});
-    // response.write('<h1>Welcome to Node.js!</h1>'); //you can add this in the end function as well 
-    // response.end(); // if you dont do this the page reloads in an infinite loop 
-    if(request.method === 'GET'){
-       response.end('GET'); 
-    }
-    if(request.method === 'POST'){
-        response.end('POST'); 
-     }
-     if(request.method === 'PUT'){
-        response.end('PUT'); 
-     }
-     if(request.method === 'DELETE'){
-        response.end('DELETE'); 
-     }
     
+    if (request.url === '/get'){
+        response.write('You reached the GET page');
+        if(request.method === 'GET'){
+            response.end('GET'); 
+        }
+    } else if (request.url == '/post'){
+        response.write('You reached the POST page');
+        if(request.method === 'POST'){
+            response.end('POST'); 
+        }
+    } else if (request.url == '/put'){
+        response.write('You reached the PUT page');
+        if(request.method === 'PUT'){
+            response.end('PUT'); 
+        }
+    } else if (request.url == '/delete'){
+        response.write('You reached the DELETE page');
+        if(request.method === 'DELETE'){
+            response.end('DELETE'); 
+        }
+    } else {
+        response.end('None Found');
+    }
+});
 
-})//.listen(3000, () => console.log("http://localhost:3000")); this will work too
 server.listen(3000, () => console.log("http://localhost:3000"));
-
